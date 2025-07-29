37d593a646afd8c917ef95f6bba35086a21ce245
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-07-29 15:41:23 +0530

    BFS - Level order Traversal

---
+++ Binary Tree/bfs.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    public: 
+    int data;
+    Node* left;
+    Node* right;
+
+    public:
+    Node(int val){
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+vector<vector<int>> levelOrderTraversal(Node* root){
+    vector<vector<int>> ans;
+    if(root == NULL) return ans;
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+        int size = q.size();
+        vector<int> level;
+        for(int i = 0; i<size; i++){
+            Node* node = q.front();
+            q.pop();
+            if(node->left != NULL) q.push(node->left);
+            if(node->right != NULL) q.push(node->right);
+            level.push_back(node->data);
+        }
+        ans.push_back(level);
+    }
+    return ans;
+}
+
+// TC - O(N) SC - O(N) 
+
+int main(){
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    vector<vector<int>> ans = levelOrderTraversal(root);
+
+    for(auto it: ans) {
+        for(auto ele: it){
+            cout << ele << " ";
+        }
+        cout << endl;
+    }
+  
+}
