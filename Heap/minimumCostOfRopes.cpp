31fe4545f4094448fba124caa34bed490dc37ccd
Bhupesh-aher <aherbhupesh18@gmail.com>
2025-08-26 23:55:15 +0530

    Minimum cost to connect ropes

---
+++ Heap/minimumCostOfRopes.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+long long minCost(vector<long long> &arr){
+    priority_queue<long long, vector<long long>, greater<long>> pq;
+    for(int i = 0; i<arr.size(); i++){
+        pq.push(arr[i]);
+    }
+
+    long long cost = 0;
+
+    while(pq.size() > 1){
+        long long a = pq.top();
+        pq.pop();
+
+        long long b = pq.top();
+        pq.pop();
+
+        long long sum = a + b;
+        cost += sum;
+        pq.push(sum);
+    }
+    return cost;
+}
+
+int main(){
+
+    vector<long long> arr = {4, 3, 2, 6};
+    cout << "Minimum cost to connect the ropes is " << minCost(arr);
+    
+} 
+
+
+
+
